ec04ec0d418114f4e86b1e1ab5de2ca3a0fdc647
xjzai1 <gys54818901@gmail.com>
2026-03-13 23:41:26 +0800

    Frontend User Module Completed

--- Xjzai1-picture-frontend/src/components/GlobalHeader.vue
+++ Xjzai1-picture-frontend/src/components/GlobalHeader.vue
@@ -20,7 +20,24 @@
       <a-col flex="120px">
         <div class="user-login-status">
           <div v-if="loginUserStore.loginUser.id">
-            {{ loginUserStore.loginUser.userName ?? '无名' }}
+            <a-dropdown>
+                <a-space wrap :size="8">
+                  <a-avatar>
+                    <template #icon>
+                      <img src="https://huacheng.gz-cmc.com/upload/news/image/2023/05/26/3e67c105f5ac4a38b45a2c7f0a40688f.jpeg" alt="头像"/>
+                    </template>
+                  </a-avatar>
+                  {{ loginUserStore.loginUser.userName ?? '无名' }}
+                </a-space>
+              <template #overlay>
+                <a-menu>
+                  <a-menu-item @click="doLogout">
+                    <LogoutOutlined />
+                    退出登录
+                  </a-menu-item>
+                </a-menu>
+              </template>
+            </a-dropdown>
           </div>
           <div v-else>
             <a-button type="primary" href="/user/login">登录</a-button>
@@ -31,14 +48,17 @@
   </div>
 </template>
 <script lang="ts" setup>
-import { h, onMounted, ref } from 'vue'
+import { DownOutlined } from '@ant-design/icons-vue'
+import { LogoutOutlined } from '@ant-design/icons-vue';
+import { computed, h, onMounted, ref } from 'vue'
 import { HomeOutlined } from '@ant-design/icons-vue'
-import { MenuProps } from 'ant-design-vue'
+import { MenuProps, message } from 'ant-design-vue'
 
 const loginUserStore = useLoginUserStore()
 
 const current = ref<string[]>(['home'])
-const items = ref<MenuProps['items']>([
+// 菜单列表
+const originItems = [
   {
     key: '/',
     icon: () => h(HomeOutlined),
@@ -46,39 +66,77 @@ const items = ref<MenuProps['items']>([
     title: '主页',
   },
   {
-    key: '/about',
-    label: '关于',
-    title: '关于',
+    key: '/admin/userManage',
+    label: '用户管理',
+    title: '用户管理',
   },
   {
     key: 'others',
     label: h('a', { href: 'https://www.codefather.cn', target: '_blank' }, '编程导航'),
     title: '编程导航',
   },
-])
+]
+
+// 过滤菜单项
+const filterMenus = (menus = [] as MenuProps['items']) => {
+  return menus?.filter((menu) => {
+    if (menu.key.startsWith('/admin')) {
+      const loginUser = loginUserStore.loginUser
+      if (!loginUser || loginUser.userRole !== "admin") {
+        return false
+      }
+    }
+    return true
+  })
+}
+
+// 展示在菜单的路由数组
+const items = computed<MenuProps['items']>(() => filterMenus(originItems))
+
+
 import { useRouter } from 'vue-router'
 import { useLoginUserStore } from '@/stores/user'
+import { userLogoutUsingPost } from '@/api/userController'
+
 const router = useRouter()
 
 router.beforeEach((to, from, next) => {
-  console.log('Navigating from', from.path, 'to', to.path);
-  current.value = [to.path];
-  next();
+  console.log('Navigating from', from.path, 'to', to.path)
+  current.value = [to.path]
+  next()
 })
 
 const doMenuClick = ({ key }: { key: string }) => {
   // ant-design中Menu组件的回调函数传来的值
-  console.log('Menu clicked, key:', key);
+  console.log('Menu clicked, key:', key)
   // router.push({
   //   path: key,
   // })
-  router.push({
-    path: key,
-  }).then(() => {
-    console.log('Navigation successful');
-  }).catch((err) => {
-    console.error('Navigation failed', err);
-  });
+  router
+    .push({
+      path: key,
+    })
+    .then(() => {
+      console.log('Navigation successful')
+    })
+    .catch((err) => {
+      console.error('Navigation failed', err)
+    })
+}
+
+// 用户注销
+const doLogout = async () => {
+  const res = await userLogoutUsingPost()
+  console.log(res)
+  if (res.data.code === 0) {
+    loginUserStore.setLoginUser({
+      userName: '未登录',
+    })
+    message.success('退出登录成功')
+    await router.push('/user/login')
+  } else {
+    message.error('退出登录失败，' + res.data.message)
+  }
 }
 
 </script>
